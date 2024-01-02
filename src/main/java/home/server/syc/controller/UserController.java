1c53c1149cbcd2b71ca162abf95bc61f636c63b5
goott3 <AlEridani@gmail.com>
2024-01-02 18:12:39 +0900

    pust test

--- src/main/java/home/server/syc/controller/UserController.java
+++ src/main/java/home/server/syc/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
-
+//test
 
 
 }
